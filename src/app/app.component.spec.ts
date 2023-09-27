cf0eb260416f522c828aa5fe0c1d3511c86c51d2
ziad <ziad.hosny@bitspire.ca>
2023-09-27 14:55:21 +0300

    format app test cases

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TaskListComponent } from './components/taskList/taskList.component';
+import { TaskComponent } from './components/task/task.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { taskListService } from './services/taskList.service';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -9,8 +14,13 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        TaskListComponent,
+        TaskComponent,
+        FooterComponent,
       ],
+      providers: [taskListService],
     }).compileComponents();
   });
 
